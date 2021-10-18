22a201d4d7d2b8f44e8a5fbbb112ec76a33e40fb
Yuno1234 <61713594+Yuno1234@users.noreply.github.com>
2021-10-19 12:00:17 +1300

    end of last workshop

--- Energy_mix/static/sketch.js
+++ Energy_mix/static/sketch.js
@@ -1,82 +1,72 @@
 // load data
+totals = []
 
 fetch('/data', { method: 'GET' })
   .then(response => response.json())
   .then(data => {
-
-    console.log(data); // <------
-    converted = []
     data.forEach(c => {
-      converted.push([c[0], (parseFloat(c[1]) + parseFloat(c[2]) + parseFloat(c[3]) + parseFloat(c[4]) + parseFloat(c[5]) + parseFloat(c[6]) + parseFloat(c[7]) + parseFloat(c[8]) + parseFloat(c[9]))])
+      totals.push([c[0], (parseFloat(c[1]) + parseFloat(c[2]) + parseFloat(c[3]) + parseFloat(c[4]) + parseFloat(c[5]) + parseFloat(c[6]) + parseFloat(c[7]) + parseFloat(c[8]) + parseFloat(c[9]))])
     });
-    console.log(converted)
-
-    console.log(converted[0][0])
-
-
-
-
+    
 
 
   });
 
-const colors = [
-  '#9a3426',
-  '#9a3426',
-  '#375724',
-  '#4f5059',
-  '#b1aa22',
-  '#1c7897',
-  '#886119',
-  '87449b',
-  '#812c3b',
-  '#1b502b'
-]
-
-const argentina = ['Argentina', '329.3280383', '474.9063305', '6.347707313', '1.980226741', '91.78546271', '20.90398373', '12.37110432', '5.936273206', '18.322']
-/* August */
-const conditions = [{
-  "name": "rain",
-  "days": 6,
-  "color": "#38669a"
-},
-{
-  "name": "partly cloudy",
-  "days": 23,
-  "color": "#8EC3E6"
-},
-
-{
-  "name": "clear",
-  "days": 2,
-  "color": "#fef017"
-}
-];
-
-const total = ['Argentina', 961.8811268200002]
-let dayCount = 31
-
-
-
-const outerRadius = 100;
-const innerRadius = 50;
-
+let data;
+  function preload() {
+    data = loadJSON('/data');
+  }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  /*
+  for (const [key, value] of Object.entries(data)) {
+    console.log(value[1]);
+  }
+  */
+  console.log(totals)
+  console.log(totals[0][1])
 }
 
 function draw() {
-  background(255);
+  const total = ['Argentina', 961.8811268200002]
+    
+  const colors = [
+    '#000',
+    '#5e638c',
+    '#a971c7',
+    '#666666',
+    '#e8d64d',
+    '#aae1e3',
+    '#bd2f34',
+    '#92d692',
+    '#d1b579',
+    '#50d4aa'
+  ]
 
+  //const argentina = ['Argentina', '329.3280383', '474.9063305', '6.347707313', '1.980226741', '91.78546271', '20.90398373', '12.37110432', '5.936273206', '18.322']
+  
+  /*
+  data.forEach(c => {
+    converted.push([c[0], (parseFloat(c[1]) + parseFloat(c[2]) + parseFloat(c[3]) + parseFloat(c[4]) + parseFloat(c[5]) + parseFloat(c[6]) + parseFloat(c[7]) + parseFloat(c[8]) + parseFloat(c[9]))])
+  });
+  */
+  
+
+
+  background('#ffffff');
   fill(0);
   noStroke();
   ellipseMode(RADIUS);
+
+  
   let angleStart = -HALF_PI; // start at the top
-  for (let c = 1; c < 10; c++) {
-    let entry = parseInt(argentina[c]);
-    fill(colors[c]);
-    let wedgeSize = map(entry, 0, total[1], 0, TAU);
+  const outerRadius = 100;
+  const innerRadius = 50;
+  for (let d = 1; d < 10; d++) {
+    let entry = parseFloat(data[4][d]);
+    fill(colors[d]);
+    let wedgeSize = map(entry, 0, totals[4][1], 0, TAU);
     let angleStop = angleStart + wedgeSize;
     arc(windowWidth / 2, windowHeight / 2, outerRadius, outerRadius, angleStart, angleStop);
     angleStart = angleStop;
@@ -86,6 +76,10 @@ function draw() {
   ellipse(windowWidth / 2, windowHeight / 2, innerRadius, innerRadius);
 
 }
+
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////
 /* const centerX = windowWidth/2;
