75197be681ef04151c300359b65f854a1ca72f49
Yuno1234 <61713594+Yuno1234@users.noreply.github.com>
2021-10-21 02:54:14 +1300

    complete donut chart

--- Energy_mix/run.py
+++ Energy_mix/run.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template, jsonify, request
 import sqlite3
 app = Flask(__name__)
 
-@app.route('/')
+ENERGYDB = 'energy_consumption.db'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    con = sqlite3.connect(ENERGYDB)
+    years = []
+    cur = con.execute('SELECT year FROM "energy-consumption-by-source-and-region" WHERE entity = "Japan"')
+    for row in cur:
+        years.append(list(row))
+    return render_template('index.html', years=years)
 
 @app.route('/data')
 def data():
-    con = sqlite3.connect('energy_consumption.db')
+    con = sqlite3.connect(ENERGYDB)
     result = []
     cur = con.execute('SELECT entity, oil, gas, coal, solar, hydro, nuclear, wind, other, biofuel FROM "energy-consumption-by-source-and-region" WHERE year = 2019 AND entity IN ("Argentina", "Australia", "Brazil", "Canada", "China", "France", "Germany", "India", "Indonesia", "Italy", "Japan", "Mexico", "South Korea", "South Africa", "Russia", "Saudi Arabia", "Turkey", "United Kingdom", "United States", "Europe")')
 
@@ -16,3 +23,46 @@ def data():
         result.append(list(row))
     con.close()
     return jsonify(result)
+
+
+""" 
+country = {}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  con = sqlite3.connect(KCALDB)
+  years = []
+  year = 1961
+  cur = con.execute('SELECT year FROM food_supply_kcal WHERE entity = "Japan"')
+  for row in cur:
+    years.append(list(row))
+
+  if request.method == 'POST':
+    year = request.form['years']
+    print(year)
+    cur = con.execute('SELECT entity, kcal FROM food_supply_kcal WHERE year = ?', (year,))
+    for row in cur:
+      country[row[0]] = getColor(row[1])
+
+  if request.method == 'GET':
+    cur = con.execute('SELECT entity, kcal FROM food_supply_kcal WHERE year = "1961"')
+    for row in cur:
+      country[row[0]] = getColor(row[1])
+
+  con.close()
+  return render_template('index.html', years=years, country=country, selected=int(year)) 
+
+
+
+
+@app.route('/data')
+def data():
+    con = sqlite3.connect(ENERGYDB)
+    result = []
+    cur = con.execute('SELECT entity, oil, gas, coal, solar, hydro, nuclear, wind, other, biofuel FROM "energy-consumption-by-source-and-region" WHERE year = 2019 AND entity IN ("Argentina", "Australia", "Brazil", "Canada", "China", "France", "Germany", "India", "Indonesia", "Italy", "Japan", "Mexico", "South Korea", "South Africa", "Russia", "Saudi Arabia", "Turkey", "United Kingdom", "United States", "Europe")')
+
+    for row in cur:
+        result.append(list(row))
+    con.close()
+    return jsonify(result)
+  """
--- Energy_mix/static/sketch.js
+++ Energy_mix/static/sketch.js
@@ -61,19 +61,22 @@ function draw() {
 
   
   let angleStart = -HALF_PI; // start at the top
-  const outerRadius = 100;
-  const innerRadius = 50;
-  for (let d = 1; d < 10; d++) {
-    let entry = parseFloat(data[4][d]);
-    fill(colors[d]);
-    let wedgeSize = map(entry, 0, totals[4][1], 0, TAU);
-    let angleStop = angleStart + wedgeSize;
-    arc(windowWidth / 2, windowHeight / 2, outerRadius, outerRadius, angleStart, angleStop);
-    angleStart = angleStop;
+  const outerRadius = 60;
+  const innerRadius = 35;
+
+  for (let c = 19; c > -1; c--) {
+    for (let d = 1; d < 10; d++) {
+      let entry = parseFloat(data[c][d]);
+      fill(colors[d]);
+      let wedgeSize = map(entry, 0, totals[c][1], 0, TAU);
+      let angleStop = angleStart + wedgeSize;
+      arc(windowWidth/2, windowHeight/2, outerRadius+30*c, outerRadius+30*c, angleStart, angleStop);
+      angleStart = angleStop;
+    }
+    // knock a hole out of the middle
+    fill(255);
+    ellipse(windowWidth/2, windowHeight/2, innerRadius+30*c, innerRadius+30*c);
   }
-  // knock a hole out of the middle
-  fill(255);
-  ellipse(windowWidth / 2, windowHeight / 2, innerRadius, innerRadius);
 
 }
 
@@ -82,59 +85,9 @@ function draw() {
 
 
 ////////////////////////////////////////////////////////////////////////////////////
-/* const centerX = windowWidth/2;
-const centerY = windowHeight/2;
-
-const outerRadius = 95;
-const innerRadius = 55;
-
-//August
-
-const conditions = [{
-    "name": "rain",
-    "days": 6,
-    "color": "#38669a"
-  },
-  {
-    "name": "partly cloudy",
-    "days": 23,
-    "color": "#8EC3E6"
-  },
-
-  {
-    "name": "clear",
-    "days": 2,
-    "color": "#fef017"
-  }
-];
-
-let dayCount= 31
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(255);
-
-  fill(0);
-  noStroke();
-  ellipseMode(RADIUS);
-  let angleStart = -HALF_PI; // start at the top
-  for (let c = 0; c < conditions.length; c++) {
-    let entry = conditions[c];
-    fill(entry.color);
-    let wedgeSize = map(entry.days, 0, dayCount, 0, TAU);
-    let angleStop = angleStart + wedgeSize;
-    arc(windowWidth/2, windowHeight/2, outerRadius, outerRadius, angleStart, angleStop);
-    angleStart = angleStop;
-  }
-  // knock a hole out of the middle
-  fill(255);
-  ellipse(windowWidth/2, windowHeight/2, innerRadius, innerRadius);
 
 
-
+/*
   // draw the legend
   let legendX = 300;
   let y = 280;
